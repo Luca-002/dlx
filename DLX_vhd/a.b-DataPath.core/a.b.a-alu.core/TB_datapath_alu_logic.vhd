5d1cc18a390f6005481c1faa4eba211bd28a417b
Capitan-Uncino <dellicolli.alessio@gmail.com>
2025-08-01 18:11:17 +0200

    logic test

--- DLX_vhd/a.b-DataPath.core/a.b.a-alu.core/TB_datapath_alu_logic.vhd
+++ DLX_vhd/a.b-DataPath.core/a.b.a-alu.core/TB_datapath_alu_logic.vhd
@@ -3,6 +3,10 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
+library work;
+use work.myTypes.all;   -- This imports aluOp type
+
+
 entity TB_logic is
 end TB_logic;
 
@@ -25,14 +29,14 @@ architecture TEST of TB_logic is
 
 begin
 	add: logic_unit
-	port map(A, B, op);
+	port map(A, B, op, result );
 
-	A <= x"00000000", x"12345678" after 1 ns, x"FFFFFFFF" after 2 ns, 
-         x"80000000" after 3 ns, x"AAAAAAAA" after 4 ns, x"0F0F0F0F" after 5 ns; 
+	A <= x"AAAAAAAA", x"AAAAAAAA" after 1 ns, x"AAAAAAAA" after 2 ns, 
+         x"AAAAAAAA" after 3 ns, x"AAAAAAAA" after 4 ns, x"AAAAAAAA" after 5 ns; 
 
 
-	B <= x"00000000", x"87654321" after 1 ns, x"00000001" after 2 ns, 
-         x"80000000" after 3 ns, x"55555555" after 4 ns, x"F0F0F0F0" after 5 ns;
+	B <= x"DB6DB6DB", x"DB6DB6DB" after 1 ns, x"DB6DB6DB" after 2 ns, 
+         x"DB6DB6DB" after 3 ns, x"DB6DB6DB" after 4 ns, x"DB6DB6DB" after 5 ns;
 
 
 	op <= ALU_AND , ALU_NAND after 1 ns, ALU_OR after 2 ns, 
@@ -40,49 +44,31 @@ begin
 
 	process
 	begin
-		wait for 1.5 ns;
-		assert result = x"99999999" 
-			report "Test 1 failed: Expected result=99999999" severity error;
+		
 
-		wait for 1 ns;
-		assert result = x"00000000" 
-			report "Test 2 failed: Expected result=00000000" severity error;
+    wait for 0.5 ns;
+    assert result = x"8A28A28A"
+        report "AND Test failed: Expected result = 8A28A28A" severity error;
 
-		wait for 1 ns;
-		assert result = x"00000000" 
-			report "Test 3 failed: Expected result=00000000" severity error;
+    wait for 1 ns;
+    assert result = x"75D75D75"
+        report "NAND Test failed: Expected result = 75D75D75" severity error;
 
-		wait for 1 ns;
-		assert result = x"FFFFFFFF" 
-			report "Test 4 failed: Expected result=FFFFFFFF" severity error;
+    wait for 1 ns;
+    assert result = x"FBEFBEFB"
+        report "OR Test failed: Expected result = FBEFBEFB" severity error;
 
-		wait for 1 ns;
-		assert result = x"FFFFFFFF" 
-			report "Test 5 failed: Expected result=FFFFFFFF" severity error;
+    wait for 1 ns;
+    assert result = x"04104104"
+        report "NOR Test failed: Expected result = 04104104" severity error;
 
-		wait for 1 ns;
-		assert result = x"FFFFFFFF" 
-			report "Test 6 failed: Expected result=FFFFFFFF" severity error;
+    wait for 1 ns;
+    assert result = x"71C71C71"
+        report "XOR Test failed: Expected result = 71C71C71" severity error;
 
-		wait for 1 ns;
-		assert result = x"A9AC79AD" 
-			report "Test 7 failed: Expected result=A9AC79AD" severity error;
-
-		wait for 1 ns;
-		assert result = x"FFFFFFFF"
-			report "Test 8 failed: Expected result=FFFFFFFF" severity error;
-
-		wait for 1 ns;
-		assert result = x"0000000E" 
-			report "Test 9 failed: Expected result=0000000E" severity error;
-
-		wait for 1 ns;
-		assert result = x"00001FBE" 
-			report "Test 10 failed: Expected result=00001FBE" severity error;
-
-		wait for 1 ns;
-		assert result = x"FFFFEFC0" 
-			report "Test 11 failed: Expected result=FFFFEFC0" severity error;
+    wait for 1 ns;
+    assert result = x"8E38E38E"
+        report "NXOR Test failed: Expected result = 8E38E38E" severity error;
 		wait;
 	end process;
 
@@ -90,8 +76,8 @@ end TEST;
 
 configuration LOGICTEST of TB_logic is
   for TEST
-    for all: logic
-      use entity WORK.logic;
+    for all: logic_unit
+      use entity WORK.logic_unit;
     end for;
   end for;
 end LOGICTEST;
