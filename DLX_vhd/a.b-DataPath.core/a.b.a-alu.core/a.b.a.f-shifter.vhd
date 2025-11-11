5eaf5526007ce95aabc664823bb182fe45ad5282
Luca-002 <lucagenca02@gmail.com>
2025-11-11 15:57:57 +0100

    fixed shifter

--- DLX_vhd/a.b-DataPath.core/a.b.a-alu.core/a.b.a.f-shifter.vhd
+++ DLX_vhd/a.b-DataPath.core/a.b.a-alu.core/a.b.a.f-shifter.vhd
@@ -16,22 +16,132 @@ entity shifter is
 end entity shifter;
 
 architecture BEHAVIORAL of shifter is
-    function rotate_left(val: std_logic_vector; shamt: integer) return std_logic_vector is
-        variable temp: std_logic_vector(N-1 downto 0);
-    begin
-        temp := val(N-1-shamt downto 0) & val(N-1 downto N-shamt);
-        return temp;
-    end function;
+    component MUX21_GENERIC is
+        generic (NBIT: integer:= N);
+			Port (	A:	In	std_logic_vector(N-1 downto 0);
+				B:	In	std_logic_vector(N-1 downto 0);
+				SEL:	In	std_logic;
+				Y:	Out	std_logic_vector(N-1 downto 0));
+		end component;
+    function log(N: integer) return integer is
+		variable result: integer := 0;
+		variable tmp: integer := N;
+	begin
+		while tmp > 1 loop
+			tmp := tmp / 2;
+			result := result + 1;
+		end loop;
+		return result;
+	end function;
+    type matrix is array (log(N)-1 downto 0) of std_logic_vector(N-1 downto 0);
+    signal intermediatelsl: matrix;
+	signal tmplsl: matrix; 
+	signal intermediatelsr: matrix;
+	signal tmplsr: matrix; 
+    signal intermediateasr: matrix;
+	signal tmpasr: matrix;
+	signal intermediaterl: matrix;
+	signal tmprl: matrix; 
+	signal intermediaterr: matrix;
+	signal tmprr: matrix; 
 
-    function rotate_right(val: std_logic_vector; shamt: integer) return std_logic_vector is
-        variable temp: std_logic_vector(N-1 downto 0);
-    begin
-        temp := val(shamt-1 downto 0) & val(N-1 downto shamt);
-        return temp;
-    end function;
 begin
 
-    SHIFT: process (A, B, LOGIC_ARITH, LEFT_RIGHT, SHIFT_ROTATE)
+    L_rotate: for i in 0 to log(N)-1 generate
+    
+    	begin
+    	first: if i=0  generate
+    		begin
+    			intermediaterl(i)<=A(N-2 downto 0)&A(N-1 downto N-1);
+    		first_mux: MUX21_GENERIC
+                      generic map (NBIT=>N)
+                      port map ( intermediaterl(i),A,B(i),tmprl(0));
+    	end  generate first;
+    	not_first: if i>0 generate
+    		begin
+    			intermediaterl(i)<=tmprl(i-1)(N-(2**i)-1 downto 0)&tmprl(i-1)(N-1 downto N-(2**i));
+    		other_muxes: MUX21_GENERIC
+    		generic map (NBIT=>N)	
+    		port map(intermediaterl(i),tmprl(i-1) , B(i),tmprl(i));
+    	end generate not_first;
+    end generate L_rotate;
+            
+            
+    R_rotate: for i in 0 to log(N)-1 generate
+            
+    	begin
+    	first: if i=0  generate
+    		begin
+    			intermediaterr(i)<=A(0 downto 0)&A(N-1 downto 1);
+    		first_mux: MUX21_GENERIC
+                      generic map (NBIT=>N)
+                      port map ( intermediaterr(i),A,B(i),tmprr(0));
+    	end  generate first;
+    	not_first: if i>0 generate
+    		begin
+    			intermediaterr(i)<=tmprr(i-1)((2**i)-1 downto 0 )&tmprr(i-1)(N-1 downto (2**i));
+    		other_muxes: MUX21_GENERIC
+    		generic map (NBIT=>N)	
+    		port map(intermediaterr(i),tmprr(i-1) , B(i),tmprr(i));
+    	end generate not_first;
+    end generate R_rotate;
+    
+L_shifter: for i in 0 to log(N)-1 generate
+	
+	begin
+	first: if i=0  generate
+		begin
+		intermediatelsl(i)<=A(N-2 downto 0)&'0';
+		first_mux: MUX21_GENERIC
+                  generic map (NBIT=>N)
+                  port map ( intermediatelsl(i),A,B(i),tmplsl(0));
+	end  generate first;
+	not_first: if i>0 generate
+		begin
+		intermediatelsl(i)<= tmplsl(i-1)(N-(2**i)-1 downto 0)&((2**i)-1 downto 0 => '0');
+		other_muxes: MUX21_GENERIC
+		generic map (NBIT=>N)	
+		port map(intermediatelsl(i),tmplsl(i-1) , B(i),tmplsl(i));
+	end generate not_first;
+end generate L_shifter;
+
+    R_shifter_l: for i in 0 to log(N)-1 generate
+            
+    	begin
+    	first: if i=0  generate
+    		begin
+    		intermediatelsr(i)<='0'&A(N-1 downto 1);
+    		first_mux: MUX21_GENERIC
+                      generic map (NBIT=>N)
+                      port map ( intermediatelsr(i),A,B(i),tmplsr(0));
+    	end  generate first;
+    	not_first: if i>0 generate
+    		begin
+    		intermediatelsr(i)<=((2**i)-1 downto 0 => '0')&tmplsr(i-1)(N-1 downto (2**i));
+    		other_muxes: MUX21_GENERIC
+    		generic map (NBIT=>N)	
+    		port map(intermediatelsr(i),tmplsr(i-1) , B(i),tmplsr(i));
+    	end generate not_first;
+    end generate R_shifter_l;  
+    R_shifter_a: for i in 0 to log(N)-1 generate
+            
+    	begin
+    	first: if i=0  generate
+    		begin
+    		intermediateasr(i)<=A(N-1)&A(N-1 downto 1);
+    		first_mux: MUX21_GENERIC
+                      generic map (NBIT=>N)
+                      port map ( intermediateasr(i),A,B(i),tmpasr(0));
+    	end  generate first;
+    	not_first: if i>0 generate
+    		begin
+    		intermediateasr(i)<=((2**i)-1 downto 0 => tmpasr(i-1)(N-1))&tmpasr(i-1)(N-1 downto (2**i));
+    		other_muxes: MUX21_GENERIC
+    		generic map (NBIT=>N)	
+    		port map(intermediateasr(i),tmpasr(i-1) , B(i),tmpasr(i));
+    	end generate not_first;
+    end generate R_shifter_a;     
+    SHIFT: process (A, B, LOGIC_ARITH, LEFT_RIGHT, SHIFT_ROTATE,tmpasr,tmprr,tmprl,tmplsr,tmplsl)
         variable shift_amt: integer := 0;
         variable temp: std_logic_vector(N-1 downto 0);
     begin
@@ -41,9 +151,9 @@ begin
         if SHIFT_ROTATE = '0' then
             -- Rotate
             if LEFT_RIGHT = '0' then
-                temp := rotate_right(A, shift_amt);
+                OUTPUT <= tmprr(log(N)-1);
             else
-                temp := rotate_left(A, shift_amt);
+                OUTPUT <= tmprl(log(N)-1);
             end if;
         else
             -- Shift
@@ -51,22 +161,18 @@ begin
                 -- Right shift
                 if LOGIC_ARITH = '0' then
                     -- Arithmetic
-                    temp := std_logic_vector(shift_right(signed(A), shift_amt));
+                    OUTPUT <= tmpasr(log(N)-1);
                 else
                     -- Logical
-                    temp := std_logic_vector(shift_right(unsigned(A), shift_amt));
+	                OUTPUT <= tmplsr(log(N)-1);
                 end if;
             else
                 -- Left shift 
-                if LOGIC_ARITH = '0' then
-                    temp := std_logic_vector(shift_left(signed(A), shift_amt));
-                else
-                    temp := std_logic_vector(shift_left(unsigned(A), shift_amt));
-                end if;
+	            OUTPUT <= tmplsl(log(N)-1);
             end if;
         end if;
 
-        OUTPUT <= temp;
+
     end process;
 
 end architecture BEHAVIORAL;
